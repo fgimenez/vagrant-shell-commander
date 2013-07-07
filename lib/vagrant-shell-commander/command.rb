1c029bd66b7acedda50a2c07029a22d4b87c7ced
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-07-07 11:50:11 +0200

    More on command spec wording; first passing command spec

--- lib/vagrant-shell-commander/command.rb
+++ lib/vagrant-shell-commander/command.rb
@@ -1,6 +1,8 @@
 module VagrantShellCommander
   # Main plugin command
   class Command < Vagrant.plugin("2", "command")
-    
+    def execute
+      0
+    end
   end
 end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,29 +1,33 @@
 require 'spec_helper'
 
 describe VagrantShellCommander::Command do
-  let(:subject) {described_class.new}
+  let(:subject) {described_class.new('2', 'command')}
 
-  describe "options" do
-    it 'has a help option'
+  describe '#execute' do
+    it 'returns 0 if all went good' do
+      expect(subject.execute).to eql(0)
+    end
     
-    it 'has a cwd option'
-
-    it 'has a command option'
+    describe "options" do
+      it 'has a help option'
+      
+      it 'has a cwd option'
+      
+      it 'has a command option'
+    end
     
-    it 'has a vm option'
-  end
-
-  describe 'error reporting' do
-    it 'reports an error if the given vm is not defined'
+    describe 'error reporting' do
+      it 'reports an error if the given vm is not defined'
+      
+      it 'reports information for every non-running machine'
+    end
     
-    it 'reports an error if none vm is running'
-  end
-
-  describe 'command execution' do
-    it 'executes the given command'
-
-    it 'executes the given command on every vm if vm option is missing'
-
-    it 'shows the help when no command is given'
+    describe 'command execution' do
+      it 'executes the given command'
+      
+      it 'executes the given command on every vm if vm option is missing'
+      
+      it 'shows the help when no command is given'
+    end
   end
 end
