7a03bc90f0df22e88d0abce63e95010f5b976dec
Federico Gimenez Nieto <federico.gimenez@gmail.com>
2013-09-15 08:43:39 +0200

    Config option after_boot

--- lib/vagrant-shell-commander/config.rb
+++ lib/vagrant-shell-commander/config.rb
@@ -1,5 +1,19 @@
 module VagrantShellCommander
+  # Configuration options
   class Config < Vagrant.plugin("2", "config")
-    attr_accessor :sh
+    attr_accessor :after_boot
+
+    # Initialize override, setting config options default values
+    # for merging
+    def initialize
+      super
+
+      @after_boot = UNSET_VALUE
+    end
+
+    # finalize! override, unseting config options
+    def finalize!
+      @after_boot = nil if @after_boot == UNSET_VALUE
+    end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,11 +2,34 @@ require 'spec_helper'
 
 describe VagrantShellCommander::Config do
 
-  it 'should have a sh attr_accessor' do
-    object = described_class.new
+  let(:subject) {described_class.new}
 
-    expect(object).to respond_to(:sh)
-    expect(object).to respond_to(:sh=)
+  describe 'config properies' do
+    it 'should have a after_boot attr_accessor' do
+      expect(subject).to respond_to(:after_boot)
+      expect(subject).to respond_to(:after_boot=)
+    end
   end
 
+  describe "#initialize" do
+    it 'should initialize after_boot' do
+      expect(subject.after_boot).to be_equal(Vagrant::Plugin::V2::Config::UNSET_VALUE)
+    end
+  end
+
+  describe "#finalize!" do
+    it 'should deinitialize after_boot if it has been set' do
+      config = described_class.new
+      config.finalize!
+      expect(config.after_boot).to be_nil
+    end
+
+    it 'should not deinitialize after_boot if it has not been set' do
+      config = described_class.new
+      config.after_boot = 'value'
+      config.finalize!
+      expect(config.after_boot).to eq('value')
+    end
+
+  end
 end
